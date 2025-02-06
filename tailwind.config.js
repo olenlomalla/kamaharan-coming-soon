28d0590e0b81f1916793d3e562e9a0e1f6a229f3
DevReact34 <werring008@gmail.com>
2025-02-06 16:23:36 +0200

    changed mobile version coming-soon-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,9 +30,20 @@ export default {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
+        modalIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
       animation: {
         fadeIn: "fadeIn 0.5s ease-in-out",
+        modalIn: "modalIn 0.5s ease-out",
       },
     },
   },

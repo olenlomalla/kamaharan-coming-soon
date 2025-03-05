e8f527c38095db594a1ed166abfcb53cd5d64bf5
Igor Polyakov <igor@polyakov.org>
2025-03-05 16:20:54 +0200

    fix: remove modal-related code since using direct HubSpot link

--- src/pages/ComingSoonPage.tsx
+++ src/pages/ComingSoonPage.tsx
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import GradientBackground from "@/components/coming-soon-page/background/GradientBackground";
 import UserLogin from "@/components/login/UserLogin";
 // import BottomIcons from "@/components/common/BottomIcons";
-import ComingSoonModal from "@/components/modal/ComingSoonModal/ComingSoonModal";
 import ComingSoonPageLayout from "@/layouts/ComingSoonPage/ComingSoonPageLayout";
 import { initGA } from "@/utils/analytics";
 
 const ComingSoonPage: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   useEffect(() => {
     initGA();
   }, []);
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
   return (
     <div className="relative bg-black max-h-[100svh] overflow-hidden text-white">
       <GradientBackground />
       <UserLogin />
-      <ComingSoonPageLayout setModalOpen={setIsModalOpen} />
-      {isModalOpen && <ComingSoonModal onClose={handleModalClose} />}
+      <ComingSoonPageLayout />
       {/* <BottomIcons isModalOpen={isModalOpen} /> */}
     </div>
   );

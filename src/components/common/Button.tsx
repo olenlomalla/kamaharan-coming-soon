f6d6b454d598ef975e1eed318d3d2ea45f411b7d
DevReact34 <werring008@gmail.com>
2025-03-20 19:33:25 +0200

    fixed build problem

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,4 +1,6 @@
-const Button = ({ children }) => {
+import { ButtonProps } from "@/types/common";
+
+const Button = ({ children }: ButtonProps) => {
   return (
     <button className="bg-[#B8A888] rounded-[40px] text-[#F8F7F4] h-[64px] w-[200px] py-4 px-8 font-body text-lg">
       {children}

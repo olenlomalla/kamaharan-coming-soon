9ad7672867877f900aea6c69db4f3d97b8411f6c
Kamaharan1 <mohamed_ali100@hotmail.co.uk>
2025-04-05 10:11:20 +0100

    Add Brothers Umrah page

---
+++ pages/brothersumrah.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import type { NextPage } from "next";
+
+const BrothersUmrah: NextPage = () => {
+  return (
+    <div>
+      <h1>Brothers Umrah Page</h1>
+      {/* Content from Brothers Umrah repo */}
+    </div>
+  );
+};
+
+export default BrothersUmrah;

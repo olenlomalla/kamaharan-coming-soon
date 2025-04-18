f032d57dac0731cf5b406e13814776a84e8d9e7e
Kamaharan1 <mohamed_ali100@hotmail.co.uk>
2025-04-18 23:06:09 +0100

    Ultimate

---
+++ src/pages/brothersumrah.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+
+import Footer from "../components/brothersumrah/Footer";
+import Header from "../components/brothersumrah/Header";
+
+const BrothersUmrah: FC = () => {
+  return (
+    <div className="bg-umrah-cream min-h-screen">
+      <Header />
+      <div className="container-custom mt-16 py-20">
+        <h1 className="mb-6 text-3xl font-bold">Brothers Umrah</h1>
+        <p className="mb-4">Welcome to the Brothers Umrah page.</p>
+        {/* Add more content here */}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default BrothersUmrah;

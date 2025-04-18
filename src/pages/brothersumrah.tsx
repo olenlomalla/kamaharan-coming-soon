51598021201cbc3349a34cdda5279f93cedb3d70
Kamaharan1 <mohamed_ali100@hotmail.co.uk>
2025-04-19 00:14:53 +0100

    BUSRCPG

--- src/pages/brothersumrah.tsx
+++ src/pages/brothersumrah.tsx
@@ -2,14 +2,9 @@ import { FC } from "react";
 
 const BrothersUmrah: FC = () => {
   return (
-    <div className="bg-umrah-cream min-h-screen">
-      <Header />
-      <div className="container-custom mt-16 py-20">
-        <h1 className="mb-6 text-3xl font-bold">Brothers Umrah</h1>
-        <p className="mb-4">Welcome to the Brothers Umrah page.</p>
-        {/* Add more content here */}
-      </div>
-      <Footer />
+    <div>
+      <h1>Brothers Umrah</h1>
+      <p>Welcome to the Brothers Umrah page.</p>
     </div>
   );
 };

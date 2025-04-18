7fe8c15aaec8aadaa2c05b0ce91c1301ff987d37
Kamaharan1 <mohamed_ali100@hotmail.co.uk>
2025-04-18 23:57:00 +0100

    BUSRCPG

---
+++ src/pages/brothersumrah.ts
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const BrothersUmrah: FC = () => {
+  return (
+    <div>
+      <h1>Brothers Umrah</h1>
+      <p>Welcome to the Brothers Umrah page.</p>
+    </div>
+  );
+};
+
+export default BrothersUmrah;

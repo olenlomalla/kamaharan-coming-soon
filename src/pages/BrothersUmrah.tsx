8706a1c33f20bf4c035effaad48fd55505cdc703
Kamaharan1 <mohamed_ali100@hotmail.co.uk>
2025-04-19 13:41:14 +0100

    Comp2

--- src/pages/BrothersUmrah.tsx
+++ src/pages/BrothersUmrah.tsx
@@ -2,14 +2,11 @@ import { FC } from "react";
 
 import Benefits from "../components/brothersumrah/Benefits";
 import ContactForm from "../components/brothersumrah/ContactForm";
-import DrivingBonus from "../components/brothersumrah/DrivingBonus";
 import Experience from "../components/brothersumrah/Experience";
 import FAQ from "../components/brothersumrah/FAQ";
-import Footer from "../components/brothersumrah/Footer";
 import Header from "../components/brothersumrah/Header";
 import Hero from "../components/brothersumrah/Hero";
 import Pricing from "../components/brothersumrah/Pricing";
-import VideoShowcase from "../components/brothersumrah/VideoShowcase";
 
 const BrothersUmrah: FC = () => {
   return (
@@ -27,14 +24,8 @@ const BrothersUmrah: FC = () => {
         {/* Benefits section */}
         <Benefits />
 
-        {/* DrivingBonus section */}
-        <DrivingBonus />
-
         {/* Pricing section */}
         <Pricing />
-
-        {/* VideoShowcase section */}
-        <VideoShowcase />
 
         {/* FAQ section */}
         <FAQ />
@@ -42,9 +33,6 @@ const BrothersUmrah: FC = () => {
         {/* Contact section */}
         <ContactForm />
       </main>
-
-      {/* Footer */}
-      <Footer />
     </div>
   );
 };
